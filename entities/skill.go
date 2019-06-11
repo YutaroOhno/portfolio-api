431b9cee6688fffa0d9ca309cbfdd8e74db59121
OhnoYutaro <utr066@yahoo.co.jp>
2019-06-11 17:13:37 +0900

    フィールド名変更

--- entities/skill.go
+++ entities/skill.go
@@ -4,5 +4,5 @@ type Skill struct {
 	ID    	 int    `gorm:"primary_key" json:"id"`
 	Name 	 string `gorm:"not null" json:"name"`
 	//levelは中間テーブルにある
-	Levels   int   `json:"levels"`
+	Level   int   `json:"level"`
 }
