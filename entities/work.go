464d5ba884a964ed562caf3982ccaa1dbce262d5
OhnoYutaro <utr066@yahoo.co.jp>
2019-07-05 09:36:48 +0900

    There is strong shadow where there is much light.

--- entities/work.go
+++ entities/work.go
@@ -1,11 +1,11 @@
 package entities
 
 type Work struct {
-	ID    	    int    `gorm:"primary_key" json:"id"`
-	UserId	    int    `gorm:"not_null" json:"user_id"`
+	ID          int    `gorm:"primary_key" json:"id"`
+	UserId      int    `gorm:"not_null" json:"user_id"`
 	Title       string `gorm:"not_null" json:"title"`
 	SubTitle    string `gorm:"not_null" json:"sub_title"`
 	Description string `json:"description"`
-	URL 		string `json:"url"`
-	ImagePath 	string `json:"image_path"`
+	URL         string `json:"url"`
+	ImagePath   string `json:"image_path"`
 }
