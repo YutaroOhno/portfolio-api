ecc71ab85223fe8f765c6047ffb12e9b2db3d823
OhnoYutaro <utr066@yahoo.co.jp>
2019-07-05 09:43:22 +0900

    Growth is often a painful process.

--- usecases/skills/skill.go
+++ usecases/skills/skill.go
@@ -1,26 +1,26 @@
 package skills
 
 import (
+	"os"
 	"portfolio-api/entities"
 	"portfolio-api/infrastructure/db"
-	"portfolio-api/usecases/repositories"
+	"portfolio-api/usecases"
 	"portfolio-api/usecases/logging"
 	"portfolio-api/usecases/ports"
-	"portfolio-api/usecases"
-	"os"
+	"portfolio-api/usecases/repositories"
 )
 
 type SkillUsecase struct {
 	SkillRepository repositories.SkillRepository
-	DB             *db.DB
-	Logging 		logging.Logging
+	DB              *db.DB
+	Logging         logging.Logging
 }
 
 func (usecase *SkillUsecase) GetUserSkills(userId int) (*ports.UserSkillsOutputPort, *usecases.UError) {
 	userWithSkills, err := usecase.SkillRepository.Find(userId, usecase.DB.GormDB)
 	for i := 0; i < len(userWithSkills.Skills); i++ {
-        userWithSkills.Skills[i].ImagePath = os.Getenv("BUCKET_PATH") + userWithSkills.Skills[i].ImagePath
-    }
+		userWithSkills.Skills[i].ImagePath = os.Getenv("BUCKET_PATH") + userWithSkills.Skills[i].ImagePath
+	}
 
 	if uerr := usecases.GetUErrorByError(err); uerr != nil {
 		usecase.Logging.Error(uerr)
@@ -32,7 +32,7 @@ func (usecase *SkillUsecase) GetUserSkills(userId int) (*ports.UserSkillsOutputP
 
 func createOutputPort(userWithSkills entities.User) *ports.UserSkillsOutputPort {
 	return &ports.UserSkillsOutputPort{
-		UserID:    	userWithSkills.ID,
-		Skills:     userWithSkills.Skills,
+		UserID: userWithSkills.ID,
+		Skills: userWithSkills.Skills,
 	}
 }
