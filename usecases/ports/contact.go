91f7760d4323fcc4c6fbaa3d4084a216afc6c744
OhnoYutaro <utr066@yahoo.co.jp>
2019-07-05 09:41:42 +0900

    Man errs as long as he strives.

--- usecases/ports/contact.go
+++ usecases/ports/contact.go
@@ -1,7 +1,7 @@
 package ports
 
 type ContactInputPort struct {
-	Name 		string `json:"name"`
+	Name        string `json:"name"`
 	MailAddress string `json:"mail_address"`
-	Content 	string `json:"content"`
+	Content     string `json:"content"`
 }
