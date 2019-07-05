965dd0cb00956dfd7501531c76c01c2664252982
OhnoYutaro <utr066@yahoo.co.jp>
2019-07-05 09:40:06 +0900

    Stay hungry. Stay foolish.

--- infrastructure/server/router.go
+++ infrastructure/server/router.go
@@ -1,16 +1,16 @@
 package server
 
 import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"os"
 	"portfolio-api/infrastructure/db"
+	"portfolio-api/interfaces/api/contacts"
 	"portfolio-api/interfaces/api/profiles"
 	"portfolio-api/interfaces/api/skills"
 	"portfolio-api/interfaces/api/works"
-	"portfolio-api/interfaces/api/contacts"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
 	"portfolio-api/usecases/logging"
 	"portfolio-api/usecases/mailer"
-	"os"
 )
 
 func Run(db *db.DB, logging logging.Logging, mailer mailer.Mailer) {
@@ -24,20 +24,20 @@ func Run(db *db.DB, logging logging.Logging, mailer mailer.Mailer) {
 	}))
 
 	// ユーザーのプロフィール情報を取得
-	profileController := profiles.NewProfileController(db,logging)
+	profileController := profiles.NewProfileController(db, logging)
 	router.GET("/users/:user_id/profile", profileController.GetUserProfile)
 
 	// ユーザーのスキル情報を取得
-	skillController := skills.NewUserSkillController(db,logging)
+	skillController := skills.NewUserSkillController(db, logging)
 	router.GET("/users/:user_id/skills", skillController.GetUserSKills)
 
 	// ユーザーの作品情報を取得
-	workController := works.NewWorkController(db,logging)
+	workController := works.NewWorkController(db, logging)
 	router.GET("/users/:user_id/works", workController.GetUserWorks)
 
 	// コンタクト
 	contactController := contacts.NewContactController(mailer, logging)
 	router.POST("/users/:user_id/contact", contactController.SendMail)
-	
+
 	router.Run()
 }
