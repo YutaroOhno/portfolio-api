de426cde631984cbd911fdbe6975a03a07353690
OhnoYutaro <utr066@yahoo.co.jp>
2019-07-05 09:36:08 +0900

    Living is not breathing but doing.

--- entities/skill.go
+++ entities/skill.go
@@ -1,9 +1,9 @@
 package entities
 
 type Skill struct {
-	ID    	 	int    `gorm:"primary_key" json:"id"`
-	Name 	 	string `gorm:"not null" json:"name"`
-	ImagePath   string `json:"image_path"`
+	ID        int    `gorm:"primary_key" json:"id"`
+	Name      string `gorm:"not null" json:"name"`
+	ImagePath string `json:"image_path"`
 	//levelは中間テーブルにある
-	Level   	int    `json:"level"`
+	Level int `json:"level"`
 }
