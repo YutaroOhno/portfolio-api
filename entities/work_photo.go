1ef0107efc2b44f2aa943f9c1182845d030b5f20
OhnoYutaro <utr066@yahoo.co.jp>
2019-07-05 09:35:38 +0900

    Simplicity is the ultimate sophistication.

--- entities/work_photo.go
+++ entities/work_photo.go
@@ -1,6 +1,6 @@
 package entities
 
 type WorkPhoto struct {
-	WorkId int    	 `gorm:"primary_key" json:"id"`
+	WorkId    int    `gorm:"primary_key" json:"id"`
 	PhotoPath string `json:"photo_path"`
 }
