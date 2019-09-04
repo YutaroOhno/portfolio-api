af91ffca0db957c5d33c4b6ace98db6f7bf16524
OhnoYutaro <utr066@yahoo.co.jp>
2019-09-04 10:24:07 +0900

    herokuに合わせた

--- infrastructure/db/postgres.go
+++ infrastructure/db/postgres.go
@@ -3,7 +3,7 @@ package db
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	// "github.com/lib/pq"
+	"github.com/lib/pq"
 	"os"
 	"fmt"
 )
@@ -17,21 +17,22 @@ func NewPostgres() *Postgres {
 }
 
 func (postgres *Postgres) Open() *DB {
-	// 本番環境（heroku）を考慮
-	// var (
-	// 	connection string
-	// 	err        error
-	// )
-	// if os.Getenv("ENV") == "prod" {
-	// 	url := os.Getenv("DATABASE_URL")
-	// 	connection, err = pq.ParseURL(url)
-	// 	if err != nil {
-	// 		panic(err.Error())
-	// 	}
-	// 	connection += " sslmode=require"
-	// } else {
-	// 	connection = os.Getenv("DATABASE_URL")
-	// }
+
+	var (
+		connection string
+		err        error
+	)
+
+	if os.Getenv("ENV") == "prod" {
+		url := os.Getenv("DATABASE_URL")
+		connection, err = pq.ParseURL(url)
+		if err != nil {
+			panic(err.Error())
+		}
+		connection += " sslmode=require"
+	} else {
+		connection = os.Getenv("DATABASE_URL")
+	}
 
 	connections := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", "127.0.0.1", os.Getenv("MASTER_USER"), os.Getenv("CLOUDSQL_DBNAME"), os.Getenv("MASTER_PASSWORD")) //Build connection string
 	db, err := gorm.Open("postgres", connections)
